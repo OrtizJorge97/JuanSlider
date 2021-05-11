3137d4bbc93d057e9c3a647bc5656cf0ae11f0fb
OrtizJorge97 <jorge.ortiz.c97@gmail.com>
2021-05-11 14:22:49 -0700

    first commit

---
+++ src/components/SliderData.js
@@ -0,0 +1,17 @@
+export const SliderData = [
+    {
+        image: "https://images.unsplash.com/photo-1618827718822-98fae761b7b4?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1618840422459-78477e94f326?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1618834676583-0dc5dcd7bfac?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1618849985811-a5a96a1363b5?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1618826984804-232bdb22ad2f?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    }
+]
